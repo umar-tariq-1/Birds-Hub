960a320c3293a31f178ab3dcb267103746dd6347
umart823 <umart823@gmail.com>
2023-08-17 14:28:18 +0500

    finalized

--- frontend/src/pages/Home/homecontent.jsx
+++ frontend/src/pages/Home/homecontent.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import main from "../../utils/pictures/main.webp";
+import main from "../../utils/pictures/mainCompressed.webp";
 import styles from "./styles.module.css";
 import useImagePreloader from "../../hooks/useImagePreloader/useImagePreloader";
 import CustomLoadingAnimation from "../../components/LoadingAnimation/loadingAnimation";
@@ -14,6 +14,7 @@ export default function HomeContent() {
   const { imagesPreloaded } = useImagePreloader(preloadSrcList);
 
    if (!imagesPreloaded) {
+    console.log(imagesPreloaded);
     return <CustomLoadingAnimation overlayColor={"rgba(0,0,0,0.1)"} />;
   }
   return (
@@ -21,17 +22,17 @@ export default function HomeContent() {
       {/* <Navbar Home={1} /> */}
       <div
         style={{
-          height: "560px",
-          backgroundImage: `url(${main})`,
-          backgroundPosition: "center",
+          height: "600px",
+          backgroundImage: `url(${preloadSrcList[0]})`,
+          backgroundPosition: "81%",
           backgroundSize: "cover",
           letterSpacing:"1px"
         }}
       >
-        <div className="container h-100">
+        <div className="container h-100 d-flex justify-content-center justify-content-md-start">
           <div className="row h-100">
-            <div className="col-md-8 mb-3 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
-              <div style={{ maxWidth: "600px" }}>
+            <div className="col-md-8 mb-3 text-center d-flex justify-content-center align-items-end align-items-md-center w-100">
+              <div className="py-5" style={{ maxWidth: "1000px" }}>
                 <div className={`${styles.ani1} ${styles.animatedParagraph}`}>
                   <h1
                     className="text-uppercase text-center fw-bold"
@@ -52,11 +53,6 @@ export default function HomeContent() {
                   >
                      with Avian Inventory
                   </h1>
-                </div>
-                <div className={`${styles.ani4} ${styles.animatedParagraph}`}>
-                  <p style={{ color: "aliceblue",fontFamily:'Titillium Web, sans-serif' ,fontSize: "130%" }}>
-                    We are a team of passionate individuals who are committed to enhance bird entrepreneurs by powerful inventory. {/* them to reach wider audience and ensure fair prices */}{/*  Our mission is to bridge the gap between farmers and market. */}
-                  </p>
                 </div>
                 <div className={`${styles.ani5} ${styles.animatedParagraph}`}>
                   <Link
